6f64bd1f3d52b42e3d1ec1d2d92a36f8defe106d
21335 <ukairia777@gmail.com>
2021-12-30 15:55:08 +0900

    'text_preprocessing_chapter'

--- 02. text preprocessing/2-8. one_hot_encoding.py
+++ 02. text preprocessing/2-8. one_hot_encoding.py
@@ -13,26 +13,26 @@ Original file is located at
 
 pip install konlpy
 
+"""# 1. 원-핫 인코딩 튜토리얼"""
+
 from konlpy.tag import Okt  
+
 okt = Okt()  
-token = okt.morphs("나는 자연어 처리를 배운다")  
-print(token)
+tokens = okt.morphs("나는 자연어 처리를 배운다")  
+print(tokens)
 
-word2index = {}
-for voca in token:
-  if voca not in word2index.keys():
-    word2index[voca] = len(word2index)
-print(word2index)
+word_to_index = {word : index for index, word in enumerate(tokens)}
+print('단어 집합 :',word_to_index)
 
-def one_hot_encoding(word, word2index):
-  one_hot_vector = [0]*(len(word2index))
-  index = word2index[word]
+def one_hot_encoding(word, word_to_index):
+  one_hot_vector = [0]*(len(word_to_index))
+  index = word_to_index[word]
   one_hot_vector[index] = 1
   return one_hot_vector
 
-one_hot_encoding("자연어", word2index)
+one_hot_encoding("자연어", word_to_index)
 
-text = "나랑 점심 먹으러 갈래 점심 메뉴는 햄버거 갈래 갈래 햄버거 최고야"
+"""# 2. 케라스를 이용한 원-핫 인코딩"""
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.utils import to_categorical
@@ -41,7 +41,7 @@ text = "나랑 점심 먹으러 갈래 점심 메뉴는 햄버거 갈래 갈래 
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts([text])
-print(tokenizer.word_index) # 각 단어에 대한 인코딩 결과 출력.
+print('단어 집합 :',tokenizer.word_index)
 
 sub_text = "점심 먹으러 갈래 메뉴는 햄버거 최고야"
 encoded = tokenizer.texts_to_sequences([sub_text])[0]
