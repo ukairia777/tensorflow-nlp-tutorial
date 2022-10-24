e3e934d74e53d927aadbf47f80c5601ec2778d32
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 08:43:09 +0000

    Adding tarfile member sanitization to extractall()

--- 21. Memory Network/21-1. memory_network_(en).py
+++ 21. Memory Network/21-1. memory_network_(en).py
@@ -33,7 +33,26 @@ path = get_file('babi-tasks-v1-2.tar.gz', origin='https://s3.amazonaws.com/text-
                 'babi_tasks_1-20_v1-2.tar.gz')
 
 with tarfile.open(path) as tar:
- tar.extractall()
+ def is_within_directory(directory, target):
+     
+     abs_directory = os.path.abspath(directory)
+     abs_target = os.path.abspath(target)
+ 
+     prefix = os.path.commonprefix([abs_directory, abs_target])
+     
+     return prefix == abs_directory
+ 
+ def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+ 
+     for member in tar.getmembers():
+         member_path = os.path.join(path, member.name)
+         if not is_within_directory(path, member_path):
+             raise Exception("Attempted Path Traversal in Tar File")
+ 
+     tar.extractall(path, members, numeric_owner=numeric_owner) 
+     
+ 
+ safe_extract(tar)
  tar.close()
 
 DATA_DIR = 'tasks_1-20_v1-2/en-10k'
