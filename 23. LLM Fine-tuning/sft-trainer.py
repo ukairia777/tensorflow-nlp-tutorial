29df159bf6fffb594a6a5fd9b4ee66f362ca6563
21335 <ukairia777@gmail.com>
2024-02-17 21:22:22 +0900

    'add_sft_trainer'

--- 23. LLM Fine-tuning/sft-trainer.py
+++ 23. LLM Fine-tuning/sft-trainer.py
@@ -1,57 +1,86 @@
 import os
 import sys
 from typing import List
+
 import fire
 import torch
-from datasets import load_dataset #, Dataset
-from peft import LoraConfig, get_peft_model
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
-from trl import SFTTrainer
-import bitsandbytes as bnb
+import transformers
+from datasets import load_dataset
 
-def find_all_linear_names(model):
-    cls = bnb.nn.Linear4bit
-    lora_module_names = set()
-    for name, module in model.named_modules():
-        if isinstance(module, cls):
-            names = name.split('.')
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
-    return list(lora_module_names)
+from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
+from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
 
-def print_trainable_parameters(model):
-  """
-  Prints the number of trainable parameters in the model.
-  """
-  trainable_params = 0
-  all_param = 0
-  for _, param in model.named_parameters():
-    all_param += param.numel()
-    if param.requires_grad:
-      trainable_params += param.numel()
-  print(
-      f"trainable params: {trainable_params} || all params: {all_param} || trainables%: {100 * trainable_params / all_param}"
-  )
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    prepare_model_for_int8_training,
+    set_peft_model_state_dict
+)
+
+from transformers import  AutoTokenizer, AutoModelForCausalLM
+import wandb
+
+wandb.init(mode="disabled")
+
+
+class SavePeftModelCallback(TrainerCallback):
+    def on_save(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        checkpoint_folder = os.path.join(args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}")
+
+        kwargs["model"].save_pretrained(checkpoint_folder)
+
+        pytorch_model_path = os.path.join(checkpoint_folder, "pytorch_model.bin")
+        torch.save({}, pytorch_model_path)
+        return control
+
+
+class LoadBestPeftModelCallback(TrainerCallback):
+    def on_train_end(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        print(f"Loading best peft model from {state.best_model_checkpoint} (score: {state.best_metric}).")
+        best_model_path = os.path.join(state.best_model_checkpoint, "adapter_model.bin")
+        adapters_weights = torch.load(best_model_path)
+        model = kwargs["model"]
+        set_peft_model_state_dict(model, adapters_weights)
+        return control
 
 
 def train(
     # model/data params
-    base_model: str = "",
-    data_path: str = "conversations.json",
-    output_dir: str = "./results",
+    base_model: str = "", 
+    data_path: str = "",
+    output_dir: str = "",
     # training hyperparams
     batch_size: int = 128,
     micro_batch_size: int = 8,
     num_epochs: int = 1,
     learning_rate: float = 3e-4,
     cutoff_len: int = 4096,
+    val_set_size: int = 0,
     lr_scheduler: str = "cosine",
-    warmup_ratio: float = 0.1, 
+    warmup_steps: int = 100, 
     # lora hyperparams
     lora_r: int = 16,
     lora_alpha: int = 16,
     lora_dropout: float = 0.05,
     # from peft docs: ["q_proj", "k_proj", "v_proj", "o_proj", "fc_in", "fc_out", "wte", "gate_proj", "down_proj", "up_proj"]
     lora_target_modules: List[str] = ["gate_proj", "down_proj", "up_proj"],
+    # llm hyperparams
+    train_on_inputs: bool = False,  # if False, masks out inputs in loss
+    add_eos_token: bool = False,
+    group_by_length: bool = False,  # faster, but produces an odd training loss curve
+    resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
 ):
     if int(os.environ.get("LOCAL_RANK", 0)) == 0:
         print(
@@ -63,100 +92,239 @@ def train(
             f"num_epochs: {num_epochs}\n"
             f"learning_rate: {learning_rate}\n"
             f"cutoff_len: {cutoff_len}\n"
+            f"val_set_size: {val_set_size}\n"
             f"lr_scheduler: {lr_scheduler}\n"
-            f"warmup_ratio: {warmup_ratio}\n"
+            f"warmup_steps: {warmup_steps}\n"
             f"lora_r: {lora_r}\n"
             f"lora_alpha: {lora_alpha}\n"
             f"lora_dropout: {lora_dropout}\n"
             f"lora_target_modules: {lora_target_modules}\n"
+            f"train_on_inputs: {train_on_inputs}\n"
+            f"add_eos_token: {add_eos_token}\n"
+            f"group_by_length: {group_by_length}\n"
+            f"resume_from_checkpoint: {resume_from_checkpoint or False}\n"
         )
     assert (
         base_model
     ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
-
-    # from huggingface_hub import login
-    # login(token='[...your_token...]')
-    
     gradient_accumulation_steps = batch_size // micro_batch_size
+
     device_map = "auto"
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    print('gpu 개수:', world_size)
+    ddp = world_size != 1
+    if ddp:
+        device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+        print(device_map)
+        gradient_accumulation_steps = gradient_accumulation_steps // world_size
+        print("gradient_accumulation_steps: ", gradient_accumulation_steps)
 
-    dataset = load_dataset("json", data_files=data_path, split="train")
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        # load_in_8bit=True,
+        low_cpu_mem_usage=True,
+        torch_dtype=torch.float16,
+        device_map=device_map)
 
-    base_model = AutoModelForCausalLM.from_pretrained(base_model, low_cpu_mem_usage=True, torch_dtype=torch.bfloat16)
-    # base_model.config.use_cache = False
-    # base_model = prepare_model_for_kbit_training(base_model)
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    print(type(model))
-    print(model)
-    print("length of tokenizer:",len(tokenizer))
-
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    
     bos = tokenizer.bos_token_id
     eos = tokenizer.eos_token_id
     pad = tokenizer.pad_token_id
-    print("pre-trained model's BOS EOS and PAD token id:",bos,eos,pad," => It should be 1 2 None")
-
-    # tokenizer.pad_token_id = 0
+    print("pre-trained model's BOS EOS and PAD token id:",bos,eos,pad)
     tokenizer.padding_side = "right"
+
+    def tokenize(prompt, add_eos_token=True):
+        result = tokenizer(
+            prompt,
+            truncation=True,
+            max_length=cutoff_len,
+            padding=False,
+            return_tensors=None,
+        )
+        if (
+            result["input_ids"][-1] != tokenizer.eos_token_id
+            and len(result["input_ids"]) < cutoff_len
+            and add_eos_token
+        ):
+            result["input_ids"].append(tokenizer.eos_token_id)
+            result["attention_mask"].append(1)
+
+        result["labels"] = result["input_ids"].copy()
+
+        return result
+
+    def generate_and_tokenize_prompt(data_point):
+        document = data_point["document"]
+        output = data_point["title"]
+        full_prompt = f"당신은 주어진 본문으로부터 적절한 제목을 생성하는 제목 생성기입니다. 본문이 주어지면 제목을 만드세요.\n\n### 본문:\n{document}\n\n### 제목:\n{output}"
+        user_prompt = f"당신은 주어진 본문으로부터 적절한 제목을 생성하는 제목 생성기입니다. 본문이 주어지면 제목을 만드세요.\n\n### 본문:\n{document}\n\n### 제목:\n"
+        tokenized_full_prompt = tokenize(full_prompt)
+        
+        # 입력은 학습하지 않을 경우
+        if not train_on_inputs:
+            # user_prompt 토큰화
+            tokenized_user_prompt = tokenize(
+                user_prompt, add_eos_token=add_eos_token)
+            
+            # user_prompt 토큰화 후의 길이
+            user_prompt_len = len(tokenized_user_prompt["input_ids"])
+
+            # 종료 토큰
+            if add_eos_token:
+                user_prompt_len -= 1
+
+            tokenized_full_prompt["labels"] = [
+                -100
+            ] * user_prompt_len + tokenized_full_prompt["labels"][
+                user_prompt_len:
+            ]
+        return tokenized_full_prompt
+
     '''
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
+    # 알파카 템플릿 예시: Bingsu/ko_alpaca_data
+    def generate_and_tokenize_prompt(data_point):
+        instruction = data_point["instruction"]
+        input = data_point["input"]
+        output = data_point["output"]
+        full_prompt = f"Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n{output}"
+        user_prompt = f"Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
+        tokenized_full_prompt = tokenize(full_prompt)
+        
+        # 입력은 학습하지 않을 경우
+        if not train_on_inputs:
+            # user_prompt 토큰화
+            tokenized_user_prompt = tokenize(
+                user_prompt, add_eos_token=add_eos_token)
+            
+            # user_prompt 토큰화 후의 길이
+            user_prompt_len = len(tokenized_user_prompt["input_ids"])
+
+            # 종료 토큰
+            if add_eos_token:
+                user_prompt_len -= 1
+
+            tokenized_full_prompt["labels"] = [
+                -100
+            ] * user_prompt_len + tokenized_full_prompt["labels"][
+                user_prompt_len:
+            ]
+        return tokenized_full_prompt
     '''
+    model = prepare_model_for_int8_training(model)
 
-    # 2. Define dataset
-    def return_prompt_and_responses(samples):
-        return {
-            "prompt": "### User:\n" + samples["question"] + "\n\n### Assistant:\n",
-            "chosen": samples["chosen"],
-            "rejected": samples["rejected"],
-        }
-
-
-    # Parameters for training arguments details => https://github.com/huggingface/transformers/blob/main/src/transformers/training_args.py#L158
-    training_args = TrainingArguments(
-        num_train_epochs= num_epochs,
-        per_device_train_batch_size=micro_batch_size,
-        logging_steps=1,
-        save_steps=10,
-        save_total_limit=2,
-        gradient_accumulation_steps=gradient_accumulation_steps,
-        learning_rate=learning_rate,
-        output_dir=output_dir,
-        lr_scheduler_type=lr_scheduler,
-        warmup_ratio=warmup_ratio,
-        optim='paged_adamw_32bit',
-        bf16=True,
-        run_name="sft_trainer",
-    )
-
-    # Change the LORA hyperparameters accordingly to fit your use case
-    peft_config = LoraConfig(
-        r=128,
-        lora_alpha=16,
-        target_modules=find_all_linear_names(base_model),
-        lora_dropout=0.05,
+    config = LoraConfig(
+        r=lora_r,
+        lora_alpha=lora_alpha,
+        target_modules=lora_target_modules,
+        lora_dropout=lora_dropout,
         bias="none",
-        task_type="CAUSAL_LM",
+        task_type="CAUSAL_LM")
+
+    model = get_peft_model(model, config)
+
+    if data_path.endswith(".json") or data_path.endswith(".jsonl"):
+        data = load_dataset("json", data_files=data_path)
+    else:
+        data = load_dataset(data_path)
+
+    if resume_from_checkpoint:
+        # Check the available weights and load them
+        checkpoint_name = os.path.join(
+            resume_from_checkpoint, "pytorch_model.bin"
+        )  # Full checkpoint
+        if not os.path.exists(checkpoint_name):
+            checkpoint_name = os.path.join(
+                resume_from_checkpoint, "adapter_model.bin"
+            )  # only LoRA model - LoRA config above has to fit
+            resume_from_checkpoint = (
+                False  # So the trainer won't try loading its state
+            )
+        # The two files above have a different name depending on how they were saved, but are actually the same.
+        if os.path.exists(checkpoint_name):
+            print(f"Restarting from {checkpoint_name}")
+            adapters_weights = torch.load(checkpoint_name)
+            set_peft_model_state_dict(model, adapters_weights)
+        else:
+            print(f"Checkpoint {checkpoint_name} not found")
+
+    model.print_trainable_parameters()
+
+    if val_set_size > 0:
+        train_val = data["train"].train_test_split(
+            test_size=val_set_size, shuffle=True, seed=42
+        )
+        train_data = (
+            train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+        )
+        val_data = (
+            train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+        )
+    else:
+        train_data = data["train"].shuffle().map(generate_and_tokenize_prompt)
+        val_data = None
+        
+            
+        print('### 첫번째 샘플 출력 ###')
+        print(train_data[0])
+        print('입력:')
+        print(tokenizer.decode(train_data[0]['input_ids']))
+        first_label = train_data[0]['labels']
+        print('타입:', type(first_label))
+        first_label = [tokenizer.pad_token_id if x == -100 else x for x in first_label]
+        print('레이블:')
+        print(first_label)
+        print(tokenizer.decode(first_label))
+
+    if not ddp and torch.cuda.device_count() > 1:
+        # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
+        model.is_parallelizable = True
+        model.model_parallel = True
+
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        args=transformers.TrainingArguments(
+            per_device_train_batch_size=micro_batch_size,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            warmup_steps=warmup_steps,
+            num_train_epochs=num_epochs,
+            learning_rate=learning_rate,
+            # dataloader_num_workers=16,
+            fp16=True,
+            logging_steps=1,
+            optim="adamw_torch",
+            evaluation_strategy="steps" if val_set_size > 0 else "no",
+            save_strategy="steps",
+            eval_steps=200 if val_set_size > 0 else None,
+            save_steps=1000,
+            lr_scheduler_type=lr_scheduler,
+            output_dir=output_dir,
+            save_total_limit=2,
+            load_best_model_at_end=True if val_set_size > 0 else False,
+            ddp_find_unused_parameters=False if ddp else None,
+            group_by_length=group_by_length,
+            remove_unused_columns=True
+        ),
+        data_collator=transformers.DataCollatorForSeq2Seq(
+            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+        ),
+        # callbacks=[SavePeftModelCallback, LoadBestPeftModelCallback], # ONLY USE LoadBestPeftModelCallback if val_set_size > 0
     )
+    model.config.use_cache = False
 
-    model = get_peft_model(base_model, peft_config)
-    print_trainable_parameters(model)
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
 
-    sft_trainer = SFTTrainer(
-        base_model,
-        train_dataset=dataset,
-        tokenizer=tokenizer,
-        max_seq_length=cutoff_len,
-        formatting_func=return_prompt_and_responses,
-        args=training_args
-    )
+    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
-    sft_trainer.train() 
-    sft_trainer.save_model(output_dir)
-
-    output_dir = os.path.join(output_dir, "final_checkpoint")
-    sft_trainer.model.save_pretrained(output_dir)
+    model.save_pretrained(output_dir)
+    # model.base_model.save_pretrained(output_dir)
+    pytorch_model_path = os.path.join(output_dir, "pytorch_model.bin")
+    torch.save({}, pytorch_model_path)
     tokenizer.save_pretrained(output_dir)
+
 
 if __name__ == "__main__":
     torch.cuda.empty_cache() 
